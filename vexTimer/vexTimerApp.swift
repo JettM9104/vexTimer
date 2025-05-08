e74076ed52290d4197b42d3bb609f1b17020fec3
JMu <64600882+JettM9104@users.noreply.github.com>
2025-05-08 18:51:27 -0400

    Initial Commit

---
+++ vexTimer/vexTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  vexTimerApp.swift
+//  vexTimer
+//
+//  Created by Jett Mu on 2025-05-08.
+//
+
+import SwiftUI
+
+@main
+struct vexTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vexTimerTests/vexTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  vexTimerTests.swift
+//  vexTimerTests
+//
+//  Created by Jett Mu on 2025-05-08.
+//
+
+import Testing
+@testable import vexTimer
+
+struct vexTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
